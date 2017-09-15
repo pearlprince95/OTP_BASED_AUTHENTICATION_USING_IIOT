b59414e5f2f6d632145eb1fe975502a05e0963d3
MUTHUKUMAR P <pearlprince95@gmail.com>
2017-09-15 23:24:20 +0530

    OTP_BASED_AUTHENTICATION_USING_IIO

---
+++ OTP_BASED_AUTHENTICATION.py
@@ -0,0 +1,30 @@
+from twilio.rest import TwilioRestClient
+import paho.mqtt.client as mqtt
+import random
+ACCOUNT_SID = 'AXXX'
+AUTH_TOKEN = 'ZZZZ'
+cell = '+91xxxxxxx'
+twilio = '+xxxxxxxx'
+sms = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+def on_connect(client1, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client1.subscribe("pin_entered")
+    client1.subscribe("otp_request")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if msg.payload.decode() == "otp_request":
+        print("X")
+        otp = random.randint(1111,9999)
+        print("Y")
+        sms.messages.create(to=cell,from_=twilio,body=otp)
+        client1.publish("otp_request", otp);
+client1 = mqtt.Client()
+client1.on_connect = on_connect
+client1.on_message = on_message
+client1.connect("test.mosquitto.org", 1883, 60)
+client1.loop_forever()
+sms.loop_forever()
+
+
+
