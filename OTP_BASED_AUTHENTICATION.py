13544161725b24f70f34033f0ac8cb4c885eeba7
MUTHUKUMAR P <pearlprince95@gmail.com>
2017-09-21 09:57:13 +0530

    Update OTP_BASED_AUTHENTICATION.py

--- OTP_BASED_AUTHENTICATION.py
+++ OTP_BASED_AUTHENTICATION.py
@@ -1,16 +1,17 @@
-from twilio.rest import TwilioRestClient
+# from twilio.rest import TwilioRestClient IF WINDOWS 
+from twilio.rest import Client # IF RPI
 import paho.mqtt.client as mqtt
 import random
 ACCOUNT_SID = 'AXXX'
 AUTH_TOKEN = 'ZZZZ'
 cell = '+91xxxxxxx'
 twilio = '+xxxxxxxx'
-sms = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+sms = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)#IF WINDOWS
+sms = Client(ACCOUNT_SID, AUTH_TOKEN)#RPI
 def on_connect(client1, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client1.subscribe("pin_entered")
     client1.subscribe("otp_request")
-
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     if msg.payload.decode() == "otp_request":
@@ -25,6 +26,3 @@ client1.on_message = on_message
 client1.connect("test.mosquitto.org", 1883, 60)
 client1.loop_forever()
 sms.loop_forever()
-
-
-
